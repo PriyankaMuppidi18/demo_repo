d9f0a9706982ff59f308945742b7441c7f501d1f
Priyanka <priyanka.muppidi18@gmail.com>
2026-09-04 22:14:22 +0530

    Updated sum

--- PPS_practice/sum.c
+++ PPS_practice/sum.c
@@ -1,22 +1,25 @@
+//Code that passes an array to function and returns sum of values in an array.
 #include<stdio.h>
+
+int calculate_sum(int numbers[5]);
 
 void main()
 {
   int numbers[5];
-  for(int i = 0, i <= 5, i++)
+  for(int i = 0; i < 5; i++)
   {
-    printf("Enter value %d: ", i);
+    printf("Enter value %d: \n", i + 1);
     scanf("%d", &numbers[i]);
-    calculate_sum(number[5])
   }
+  int ans = calculate_sum(numbers);
+  printf("Their sum is: %d", ans);
 }
 
 int calculate_sum(int numbers[5])
 {
   int sum = 0;
-  for(int i = 0, i <= 5, i++)
+  for(int i = 0; i < 5; i++)
   {
-    scanf("%d", &numbers[i]);
     sum = sum + numbers[i];
   }
   return sum;
